1923c2c1854a03d4364cee6df5a91adb27fe525c
Robert Garrett <robertgarrett404@gmail.com>
2025-07-09 11:00:47 -0500

    Move circle drawing fn to a utility module

--- Series.cpp
+++ Series.cpp
@@ -5,43 +5,9 @@
 
 #include "Spinner.h"
 #include "CircularBuffer.h"
+#include "drawing_utils.h"
 #include "Helpers.h"
 #include "Fourier.h"
-
-void DrawCircle(SDL_Renderer *Renderer, int32_t _x, int32_t _y, int32_t radius)
-{
-   int32_t x = radius - 1;
-   int32_t y = 0;
-   int32_t tx = 1;
-   int32_t ty = 1;
-   int32_t err = tx - (radius << 1); // shifting bits left by 1 effectively
-                                 // doubles the value. == tx - diameter
-   while (x >= y)
-   {
-      //  Each of the following renders an octant of the circle
-      SDL_RenderDrawPoint(Renderer, _x + x, _y - y);
-      SDL_RenderDrawPoint(Renderer, _x + x, _y + y);
-      SDL_RenderDrawPoint(Renderer, _x - x, _y - y);
-      SDL_RenderDrawPoint(Renderer, _x - x, _y + y);
-      SDL_RenderDrawPoint(Renderer, _x + y, _y - x);
-      SDL_RenderDrawPoint(Renderer, _x + y, _y + x);
-      SDL_RenderDrawPoint(Renderer, _x - y, _y - x);
-      SDL_RenderDrawPoint(Renderer, _x - y, _y + x);
-
-      if (err <= 0)
-      {
-         y++;
-         err += ty;
-         ty += 2;
-      }
-      if (err > 0)
-      {
-         x--;
-         tx += 2;
-         err += tx - (radius << 1);
-      }
-   }
-}
 
 const int winWidth = 800;
 const int winHeight = 600;
---
+++ drawing_utils.h
@@ -0,0 +1,8 @@
+#ifndef DRAWING_UTILS_H_
+#define DRAWING_UTILS_H_
+
+#include <SDL2/SDL.h>
+
+void DrawCircle(SDL_Renderer *Renderer, int32_t _x, int32_t _y, int32_t radius);
+
+#endif /* DRAWING_UTILS_H_ */
